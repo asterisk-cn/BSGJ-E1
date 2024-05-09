d1137c377fdb3fc4afe89ed41da4902630390cc6
Hiroaki Suzuki <tky2302126@stu.o-hara.ac.jp>
2024-05-09 22:48:38 +0900

    add:PlayerCore.Attackでボタン入力でプレイヤーのスケールが大きくなる機能を実装 add:EnemyAttckにPlayerCharacterを追加 fix: EnemyAttck.moveで細かく振動しないように条件式を修正 確率の条件式を修正

--- Assets/Scripts/Players/PlayerCore.cs
+++ Assets/Scripts/Players/PlayerCore.cs
@@ -1,5 +1,7 @@
+using GameManagers;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Players
@@ -14,6 +16,29 @@ namespace Players
 
         CharacterParameters _defaultParameters;
         CharacterParameters _currentParameters;
+
+        [SerializeField]
+        [Header("ムキムキタイムの制限時間")]
+        public float powerUpTimeLimit;
+
+        [SerializeField]
+        [Header("大きくなる割合")]
+        public float sizeUpRate;
+
+        private enum InputPhase 
+        {
+            None,
+            WaitForNextPress,
+            WaitForNextRelease
+        }
+
+        private InputPhase _inputPhase=InputPhase.None;
+
+        private int tapCount = 0;
+
+        [SerializeField]
+        private float powerUpTimer = 0;
+
 
         // Start is called before the first frame update
         void Start()
@@ -39,11 +64,13 @@ namespace Players
             if (this.gameObject.CompareTag("Player"))
             {
                 LeftMove();
+                Attack();
             }
 
             if (this.gameObject.CompareTag("Player2"))
             {
                 RightMove();
+                //Attack();
             }
         }
 
@@ -66,7 +93,58 @@ namespace Players
 
         void Attack()
         {
+            //シーン判定
+            if (MainGameManager.instance.gameState != GameState.Fight) 
+            {
+                return;
+            }
+            else 
+            {
+                
 
+                if (powerUpTimer > powerUpTimeLimit) 
+                {
+                    Debug.Log($"スケール:{transform.localScale.x}");
+                    Debug.Log($"連打数:{tapCount}");
+                }
+                else 
+                {
+                    powerUpTimer += Time.deltaTime;
+
+                    if (_inputPhase == InputPhase.None &&
+                        Input.GetMouseButtonDown((int)MouseButton.Left)
+                        /*Input.GetKeyDown(KeyCode.Space)*/) 
+                    {
+                        tapCount++;
+                        Vector3 ScaleChange = new Vector3(sizeUpRate,sizeUpRate,sizeUpRate);
+                        this.transform.localScale += ScaleChange ;
+                        _inputPhase = InputPhase.WaitForNextRelease;
+                        Debug.Log("キー入力①");
+                    }
+
+                    if(_inputPhase ==InputPhase.WaitForNextRelease&&
+                        Input.GetMouseButtonDown((int)MouseButton.Left)==false
+                        //Input.GetKey(KeyCode.Space)==false 
+                        /*Input.GetKeyUp(KeyCode.Space)*/) 
+                    {
+                        _inputPhase =InputPhase.WaitForNextPress;
+                        Debug.Log("キーが離れた");
+                    }
+
+                    if (_inputPhase == InputPhase.WaitForNextPress &&
+                        Input.GetMouseButtonDown((int)MouseButton.Left)
+                        /*Input.GetKeyDown(KeyCode.Space)*/)
+                    {
+                        tapCount++;
+                        Vector3 ScaleChange = new Vector3(sizeUpRate, sizeUpRate, sizeUpRate);
+                        this.transform.localScale += ScaleChange; 
+                        _inputPhase = InputPhase.WaitForNextRelease;
+                        Debug.Log("キー入力②");
+                    }
+                
+                }
+
+            }
         }
     }
 }
