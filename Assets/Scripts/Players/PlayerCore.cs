09326c1b7366f25a0e038cfaf0ca1932b438c3a4
tky2302126 <157141447+tky2302126@users.noreply.github.com>
2024-05-25 01:48:16 +0900

    プレイヤー体力 #42 (#56)
    * add:体力減少用の関数を作成
    関数内にゲームオーバー処理を追加
    
    * add: player take damage
    
    ---------
    
    Co-authored-by: asterisk-cn <ttasuku-s122@g.ecc.u-tokyo.ac.jp>

--- Assets/Scripts/Players/PlayerCore.cs
+++ Assets/Scripts/Players/PlayerCore.cs
@@ -38,6 +38,8 @@ namespace Players
 
         private bool _isAttacked = false;
 
+        
+
         void Awake()
         {
             _inputs = GetComponent<PlayerInputs>();
@@ -109,6 +111,30 @@ namespace Players
                 // TODO: RunManagerを呼び出してゲーム終了処理を行う
                 MainGameManager.instance.LoadScene("Fight");
             }
+        }
+
+        //5/25追加 Suzuki H
+        //ダメージ処理用の呼び出し関数
+        public void TakeDamage(int damage)
+        {
+            _currentParameters.health -= damage;
+            //ゲームオーバー処理？　リザルト処理に遷移
+            if (_currentParameters.health <= 0)
+            {
+                Die();
+            }
+        }
+
+        void Die()
+        {
+            isAlive = false;
+            MainGameManager.instance.isClear = false;
+            SceneFadeManager.instance.FadeOut("Result");
+        }
+
+        public int GetCurrentHealth()
+        {
+            return _currentParameters.health;
         }
     }
 }
