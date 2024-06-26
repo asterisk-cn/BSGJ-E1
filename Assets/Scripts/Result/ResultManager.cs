c98350c63a3575c97c0d73f22539aa78d86ad783
asterisk-cn <ttasuku-s122@g.ecc.u-tokyo.ac.jp>
2024-06-26 21:55:32 +0900

    add: score in result

--- Assets/Scripts/Result/ResultManager.cs
+++ Assets/Scripts/Result/ResultManager.cs
@@ -2,11 +2,15 @@ using GameManagers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ResultManager : MonoBehaviour
 {
     [SerializeField] private GameObject _clearPanel;
     [SerializeField] private GameObject _gameOverPanel;
+
+    [SerializeField] private TextMeshProUGUI _scoreText;
+
     bool select = false;
 
     // Start is called before the first frame update
@@ -17,7 +21,12 @@ public class ResultManager : MonoBehaviour
 
         if (MainGameManager.instance.isClear)
         {
+            string scoreRank = MainGameManager.instance.GetScoreRank();
+            string clearTime = TimeToString(MainGameManager.instance.GetClearTime());
+            string soulDeadCount = MainGameManager.instance.GetSoulDeadCount().ToString();
+
             _clearPanel.SetActive(true);
+            _scoreText.text = "Result " + scoreRank + "\n" + "ClearTime " + clearTime + "\n" + "Dead Souls " + soulDeadCount;
         }
         else
         {
@@ -43,5 +52,13 @@ public class ResultManager : MonoBehaviour
         SceneFadeManager.instance.FadeOut("Title");
        if(!select) AudioManager.Instance.PlaySE("Button_SE");
        select =true;
+    }
+
+    private string TimeToString(float time)
+    {
+        int minute = (int)time / 60;
+        int second = (int)time % 60;
+
+        return minute.ToString() + ":" + second.ToString("00");
     }
 }
